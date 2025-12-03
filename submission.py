cada3a1dd8dacf0391127b41081f68f60845f5e3
Joe Robertazzi <robertazzijoseph@gmail.com>
2025-12-03 03:49:57 -0800

    355us, removed looping and added another block dimension instead to keep track of batches. micro optimizations in removing all divisions and modulos, replacing with bit shifts and &s.

--- submission.py
+++ submission.py
@@ -20,56 +20,60 @@ cuda_source = """
 __global__ void HistogramKernel(const uint8_t* __restrict__ data_in, int32_t* __restrict__ data_out, int length, int num_channels, int num_bins, int channels_per_batch, int num_batches) {
     extern __shared__ int32_t shared_mem[]; 
     int total_threads = blockDim.x * gridDim.x;
+    int batch = blockIdx.y; 
+    if (batch >= num_batches) return; 
 
-    for (int batch = 0; batch < num_batches; batch++) {
-        int channel_offset = batch * channels_per_batch;
+    // for (int batch = 0; batch < num_batches; batch++) {
+    int channel_offset = batch * channels_per_batch;
 
-        // clear memory
-        for (int i = threadIdx.x; i < channels_per_batch * num_bins; i += blockDim.x) {
-            shared_mem[i] = 0;
-        }
-        __syncthreads();
-
-        // calculate thread index, max vectors per row, and total vectors to compute
-        int current_thread_idx = blockDim.x * blockIdx.x + threadIdx.x;
-        int vectors_per_row = channels_per_batch >> 4;
-        // int vectors_per_row = channels_per_batch / 16; 
-        int total_vectors = length * vectors_per_row;
-
-        // loop through until all vectors are processed 
-        for (int i = current_thread_idx; i < total_vectors; i += total_threads) {
-            // int row = i / vectors_per_row;
-            // int vec_idx = i % vectors_per_row;
-            int row = i >> 2;
-            int vec_idx = i & (vectors_per_row - 1);
-            int base_local_channel = vec_idx * 16;
-            
-            int addr_offset = row * num_channels + (base_local_channel);
-
-            uchar4* vec_addr = (uchar4*)(data_in + channel_offset + addr_offset); 
-            uint4 vec_val = *reinterpret_cast<const uint4*>(vec_addr);
-            uint8_t* vec_converted = reinterpret_cast<uint8_t*>(&vec_val);
-            
-            #pragma unroll
-            for (int elem = 0; elem < 16; elem++) {
-                uint8_t val = vec_converted[elem];
-                int local_channel = base_local_channel + elem;
-                atomicAdd(&shared_mem[local_channel * num_bins + val], 1);
-            }
-        }
-        __syncthreads();
-
-        // atomic add back to global histogram
-        for (int i = threadIdx.x; i < channels_per_batch * num_bins; i += blockDim.x) {
-            int32_t count = shared_mem[i];
-            if (count > 0) {
-                int channel = channel_offset + (i / num_bins);
-                int bin = (i % num_bins);
-                atomicAdd(&data_out[channel * num_bins + bin], count);
-            }
-        }
-
+    // clear memory
+    for (int i = threadIdx.x; i < channels_per_batch * num_bins; i += blockDim.x) {
+        shared_mem[i] = 0;
     }
+    __syncthreads();
+
+    // calculate thread index, max vectors per row, and total vectors to compute
+    int current_thread_idx = blockDim.x * blockIdx.x + threadIdx.x;
+    // int vectors_per_row = channels_per_batch / 16; 
+    int vectors_per_row = channels_per_batch >> 4;
+    int total_vectors = length * vectors_per_row;
+
+    // loop through until all vectors are processed 
+    for (int i = current_thread_idx; i < total_vectors; i += total_threads) {
+        // int row = i / vectors_per_row;
+        // int vec_idx = i % vectors_per_row;
+        int row = i >> 2;
+        int vec_idx = i & (vectors_per_row - 1);
+        int base_local_channel = vec_idx * 16;
+        
+        int addr_offset = row * num_channels + (base_local_channel);
+
+        uchar4* vec_addr = (uchar4*)(data_in + channel_offset + addr_offset); 
+        uint4 vec_val = *reinterpret_cast<const uint4*>(vec_addr);
+        uint8_t* vec_converted = reinterpret_cast<uint8_t*>(&vec_val);
+        
+        #pragma unroll
+        for (int elem = 0; elem < 16; elem++) {
+            uint8_t val = vec_converted[elem];
+            int local_channel = base_local_channel + elem;
+            atomicAdd(&shared_mem[local_channel * num_bins + val], 1);
+        }
+    }
+    __syncthreads();
+
+    // atomic add back to global histogram
+    for (int i = threadIdx.x; i < channels_per_batch * num_bins; i += blockDim.x) {
+        int32_t count = shared_mem[i];
+        if (count > 0) {
+            // int channel = channel_offset + (i / num_bins);
+            // int bin = (i % num_bins);
+            int channel = channel_offset + (i >> 8); // log256
+            int bin = (i & 255); // same as mod 256
+            atomicAdd(&data_out[channel * num_bins + bin], count);
+        }
+    }
+
+    // }
 }
 
 // Host function to launch kernel
@@ -97,14 +101,14 @@ histogram_kernel(torch::Tensor data, // [length, num_channels], dtype=uint8
     const int threads = 512; 
 
     // 144 // 132 // 114 SMs. 
-    // const int blocks = (length * num_channels + (threads * channels_per_block) - 1) / (threads * channels_per_block);
-    const int blocks = 132 * 2;
+    // const int blocks = 132 * 2;
 
     // 
     size_t shared_mem_size = channels_per_batch * (num_bins) * sizeof(int32_t);
     cudaFuncSetAttribute(HistogramKernel, cudaFuncAttributeMaxDynamicSharedMemorySize, shared_mem_size);
 
     const int num_batches = (num_channels + channels_per_batch - 1) / channels_per_batch;
+    dim3 blocks(132 * 2, num_batches);
 
     // Officially launch da kernel
     HistogramKernel<<<blocks, threads, shared_mem_size>>>(data_in, data_out, length, num_channels, num_bins, channels_per_batch, num_batches);
